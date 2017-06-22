febf163cda5890dd385fbfdb7a55aec3f92a17a0
AlexsJones <tibbar@freedommail.ch>
2017-06-22 15:14:03 +0100

    updated

--- commands/node.go
+++ commands/node.go
@@ -10,13 +10,14 @@ import (
 )
 
 type PackageJSON struct {
-	Name         string            `json:"name"`
-	Version      string            `json:"version"`
-	Description  string            `json:"description"`
-	Main         string            `json:"main"`
-	Author       string            `json:"author"`
-	Scripts      map[string]string `json:"scripts"`
-	Dependencies map[string]string `json:"dependencies"`
+	Name            string            `json:"name"`
+	Version         string            `json:"version"`
+	Description     string            `json:"description"`
+	Main            string            `json:"main"`
+	Author          string            `json:"author"`
+	Scripts         map[string]string `json:"scripts"`
+	Dependencies    map[string]string `json:"dependencies"`
+	DevDependencies map[string]string `json:"devDependencies"`
 }
 
 //FixLinks ...
